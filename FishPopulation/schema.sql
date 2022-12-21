331fb7741a9e205a64c7f687e35858c615abd3b1
Alberto Abella <alberto.abella@fiware.org>
2022-12-21 02:42:45 +0100

    test of SQL export

---
+++ FishPopulation/schema.sql
@@ -0,0 +1,3 @@
+/* (Beta) Export of data model FishPopulation of the subject dataModel.Aquaculture for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+CREATE TYPE FishPopulation_type AS ENUM ('FishPopulation');
+CREATE TABLE FishPopulation (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, relatedSource json, seeAlso json, source text, type FishPopulation_type);
