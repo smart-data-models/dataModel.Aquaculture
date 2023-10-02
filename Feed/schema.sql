8d12d87289d48dc7bf6cf9fa534dea34fab62d65
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:27:13 +0200

    test of SQL export

--- Feed/schema.sql
+++ Feed/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Feed of the subject dataModel.Aquaculture for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Feed of the subject dataModel.Aquaculture for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Feed_type AS ENUM ('Feed');
-CREATE TABLE Feed (address json, alternateName text, areaServed text, dataProvider text, dateCreated timestamp, dateModified timestamp, description text, id text, location json, name text, owner json, relatedSource json, seeAlso json, source text, type Feed_type);
+CREATE TABLE Feed (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, relatedSource JSON, source TEXT, type Feed_type);
