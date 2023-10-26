fd036e22da61f246cdcc150ab35bfc37050328ba
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 13:51:26 +0200

    test of SQL export

--- Feeder/schema.sql
+++ Feeder/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Feeder of the subject dataModel.Aquaculture for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Feeder_type AS ENUM ('Feeder');
-CREATE TABLE Feeder (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, name TEXT, owner JSON, relatedSource JSON, source TEXT, type Feeder_type);
+CREATE TABLE Feeder (address JSON, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, relatedSource JSON, seeAlso JSON, source TEXT, type Feeder_type);
