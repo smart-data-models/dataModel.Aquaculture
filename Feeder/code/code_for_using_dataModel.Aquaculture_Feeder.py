608a89dd65fee7e4f7d258656b7c13302bf3792d
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:17:08 +0200

    created example of code

--- Feeder/code/code_for_using_dataModel.Aquaculture_Feeder.py
+++ Feeder/code/code_for_using_dataModel.Aquaculture_Feeder.py
@@ -24,18 +24,17 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "Feeder"
 subject = "dataModel.Aquaculture"
-relatedSource = {'type': 'Property', 'value': [{'applicationEntityId': 'urn:ngsi-ld:Feeder:items:JCKQ:85722433'}, {'applicationEntityId': 'urn:ngsi-ld:Feeder:items:QFIQ:55709596'}]}
-attribute = "relatedSource"
-value = relatedSource
-# The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
-print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
-
 print(" In case you have installed the orion-ld broker (see comments on the header of this program)")
 print(" Execute this instruction to check that the entities has been inserted")
 command = ['curl', '-X', 'GET', 'http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000']
